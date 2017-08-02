1cdc01928183700a6cb3930485261230855cc34d
liyuank <spirit84@gmail.com>
2017-08-02 13:02:42 -0700

    Add files via upload
    lock or unlock your door lock using Raspberry Pi 3 and Python

---
+++ lock.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+import time
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+
+#servo pin
+GPIO.setup(12, GPIO.OUT)
+p = GPIO.PWM(12, 50)
+
+#turn the lock first!
+p.start(2.5)
+
+#button pin
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+#led pin
+GPIO.setup(15, GPIO.OUT)
+
+
+try:
+	while True:
+		input_state = GPIO.input(13)
+    		if input_state == False:
+        		print('Button Pressed')
+			#LED on
+			GPIO.output(15,GPIO.HIGH)        		
+			time.sleep(0.2)
+
+			p.ChangeDutyCycle(7.5)
+			time.sleep(1)
+			p.ChangeDutyCycle(2.5)
+			time.sleep(3)
+			#p.ChangeDutyCycle(12.5)
+			#time.sleep(1)
+
+			#LED off
+			GPIO.output(15, GPIO.LOW)
+
+except KeyboardInterrupt:
+	p.stop()
+	GPIO.cleanup()
